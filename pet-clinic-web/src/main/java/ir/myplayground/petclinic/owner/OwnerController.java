3d102995f5b770fa321657d2e1255bdecaadd3b8
Mohamad Moein HajEsmaeili <mohamadmoein.h@gmail.com>
2022-07-16 17:15:08 +0430

    Adding Vet & Owner controller

---
+++ pet-clinic-web/src/main/java/ir/myplayground/petclinic/owner/OwnerController.java
@@ -0,0 +1,14 @@
+package ir.myplayground.petclinic.owner;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping("")
+    public String listOwners() {
+        return "owners/index";
+    }
+}
---
+++ pet-clinic-web/src/main/java/ir/myplayground/petclinic/vet/VetController.java
@@ -0,0 +1,14 @@
+package ir.myplayground.petclinic.vet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/vets")
+@Controller
+public class VetController {
+
+    @RequestMapping("")
+    public String listVets() {
+        return "vets/index";
+    }
+}
