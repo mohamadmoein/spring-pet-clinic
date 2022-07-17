7a2108e5c41dd852d512028bad42540f433bcf3e
Mohamad Moein HajEsmaeili <mohamadmoein.h@gmail.com>
2022-07-17 15:06:14 +0430

    Adding owner & vet index pages

--- pet-clinic-web/src/main/java/ir/myplayground/petclinic/vet/VetController.java
+++ pet-clinic-web/src/main/java/ir/myplayground/petclinic/vet/VetController.java
@@ -1,14 +1,22 @@
 package ir.myplayground.petclinic.vet;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping("")
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
