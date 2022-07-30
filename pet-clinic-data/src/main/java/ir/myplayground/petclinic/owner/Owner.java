68a04039510a1c1c5e756fd9e48f5f163bfacec1
Mohamad Moein HajEsmaeili <mohamadmoein.h@gmail.com>
2022-07-30 20:00:19 +0430

    Add Visit.java

--- pet-clinic-data/src/main/java/ir/myplayground/petclinic/owner/Owner.java
+++ pet-clinic-data/src/main/java/ir/myplayground/petclinic/owner/Owner.java
@@ -1,7 +1,18 @@
 package ir.myplayground.petclinic.owner;
 
 import ir.myplayground.petclinic.person.Person;
+import ir.myplayground.petclinic.pet.Pet;
+
+import java.util.Set;
 
 public class Owner extends Person {
+    private Set<Pet> pets;
 
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
