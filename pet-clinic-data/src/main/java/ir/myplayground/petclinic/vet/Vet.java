0d0fde55261ea45a3e1a5adf63ab71fbaa0077f3
Mohamad Moein HajEsmaeili <mohamadmoein.h@gmail.com>
2022-07-30 20:03:42 +0430

    Added Specialty.java for Vet.java

--- pet-clinic-data/src/main/java/ir/myplayground/petclinic/vet/Vet.java
+++ pet-clinic-data/src/main/java/ir/myplayground/petclinic/vet/Vet.java
@@ -2,6 +2,16 @@ package ir.myplayground.petclinic.vet;
 
 import ir.myplayground.petclinic.person.Person;
 
-public class Vet extends Person {
+import java.util.Set;
 
+public class Vet extends Person {
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
